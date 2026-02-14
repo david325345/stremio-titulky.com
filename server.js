62961e39a9bef822b283130e82390e54db3be1d0
david325345 <David32@seznam.cz>
2026-02-14 10:30:46 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ async function getMeta(type, id) {
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
